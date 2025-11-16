16aae58b530bbf5e50d482b5b5ba72f9b5d085a7
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-11-17 00:11:15 +0530

    update

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let fname = 'Kavishka';
 const lname = 'Piyumal';
 let fullname = `Kavishka piyumal`;
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const nameArray = ['Nimal', 'Kamal', 'Sunil'];
+const tempArray = ['ddd', 333, 'AAA'];
+const tempArr2 = [2];
+tempArr2[1] = 4;
+tempArr2[2] = false;
+tempArr2[3] = "Nimal";
+tempArr2[5] = true;
+const myObj = {
+    name1: 'Kamal',
+    name2: 'Nimal',
+    name3: 'Sunil',
+};
 let userRole;
 let testVar;
 userRole = 'Editor'; // OK
@@ -12,4 +25,6 @@ testVar = true; // OK
 console.log(userRole);
 console.log(testVar);
 console.log(fname, lname, fullname);
+console.log(tempArr2);
+console.log(myObj.name2);
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,22 @@ let fname: string = 'Kavishka';
 const lname: string = 'Piyumal';
 let fullname: string = `Kavishka piyumal`;
 
+const myArray:number[]=[1,2,3,4,5,6,7,8,9,10];
+const nameArray:string[]=['Nimal','Kamal','Sunil'];
+const tempArray:(string | number)[]=['ddd',333,'AAA'];
+const tempArr2:(number | boolean)[] | (string | number)[]=[2];
+
+tempArr2[1]=4;
+tempArr2[2]=false;
+tempArr2[3]="Nimal";
+tempArr2[5]=true;
+
+const myObj ={
+    name1: 'Kamal',
+    name2: 'Nimal',
+    name3: 'Sunil',
+}
+
 let userRole: userType;
 let testVar: test;
 
@@ -17,3 +33,5 @@ console.log(userRole);
 console.log(testVar);
 
 console.log(fname, lname, fullname);
+console.log(tempArr2);
+console.log(myObj.name2)
