24c9a24132a46e4707fad1de1c5399869552714e
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-11-18 13:55:48 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,58 @@
-class SystemUser {
-    name!: string;
-    age!: number;
-    city!: string;
-    username!: string;
-    password!: string;
+// class SystemUser {
+//     name!: string;
+//     age!: number;
+//     city!: string;
+//     username!: string;
+//     password!: string;
 
-    login() {
-        console.log('User login');
-    }
-    register() {
-        console.log('User register');
-    }
-}
+//     login() {
+//         console.log('User login');
+//     }
+//     register() {
+//         console.log('User register');
+//     }
+// }
 
-const userOne = new SystemUser();
-userOne.name = 'Kamal';
-userOne.age = 23;
-userOne.city = "Mathara";
-userOne.username = "Kamal";
-userOne.password = "kamal";
+// const userOne = new SystemUser();
+// userOne.name = 'Kamal';
+// userOne.age = 23;
+// userOne.city = "Mathara";
+// userOne.username = "Kamal";
+// userOne.password = "kamal";
 
-console.log(userOne);
-userOne.login();
+// console.log(userOne);
+// userOne.login();
 
 
-const userTwo = new SystemUser();
-userTwo.name = 'Nimal';
-userTwo.age = 27;
-userTwo.city = "Kandy";
-userTwo.username = "Nimal";
-userTwo.password = "nimal";
+// const userTwo = new SystemUser();
+// userTwo.name = 'Nimal';
+// userTwo.age = 27;
+// userTwo.city = "Kandy";
+// userTwo.username = "Nimal";
+// userTwo.password = "nimal";
 
-console.log(userTwo);
-userTwo.register();
+// console.log(userTwo);
+// userTwo.register();
+
+
+// let code: any =123;
+// let employeeCode = <number>code;
+// console.log(typeof(employeeCode));
+// console.log(employeeCode);
+
+// interface Employee {
+//     name:string;
+//     code:number;
+// }
+
+// let employee = <Employee>{}
+// employee.name = 'John';
+// employee.code = 123;
+// console.log(employee.name);
+// console.log(employee.code);
+
+let someValue: unknown ="this is a string";
+let strLength: number =(<string>someValue).length;
+
+console.log(strLength);
+
