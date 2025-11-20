8f6297f288e86ee07bbbd81b754c2f8d19d1d510
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-11-20 20:23:20 +0530

    update

--- dist/main.js
+++ dist/main.js
@@ -1,48 +1,44 @@
 "use strict";
-// class SystemUser {
-//     name!: string;
-//     age!: number;
-//     city!: string;
-//     username!: string;
-//     password!: string;
 Object.defineProperty(exports, "__esModule", { value: true });
-//     login() {
-//         console.log('User login');
-//     }
-//     register() {
-//         console.log('User register');
-//     }
-// }
-// const userOne = new SystemUser();
-// userOne.name = 'Kamal';
-// userOne.age = 23;
-// userOne.city = "Mathara";
-// userOne.username = "Kamal";
-// userOne.password = "kamal";
-// console.log(userOne);
-// userOne.login();
-// const userTwo = new SystemUser();
-// userTwo.name = 'Nimal';
-// userTwo.age = 27;
-// userTwo.city = "Kandy";
-// userTwo.username = "Nimal";
-// userTwo.password = "nimal";
-// console.log(userTwo);
-// userTwo.register();
-// let code: any =123;
-// let employeeCode = <number>code;
-// console.log(typeof(employeeCode));
-// console.log(employeeCode);
-// interface Employee {
-//     name:string;
-//     code:number;
-// }
-// let employee = <Employee>{}
-// employee.name = 'John';
-// employee.code = 123;
-// console.log(employee.name);
-// console.log(employee.code);
-let someValue = "this is a string";
-let strLength = someValue.length;
-console.log(strLength);
+class SystemUser {
+    name;
+    age;
+    city;
+    username;
+    password;
+    login() {
+        console.log('User login', this.name);
+    }
+    register() {
+        console.log('User register', this.name);
+    }
+}
+const userOne = new SystemUser();
+userOne.name = 'Kamal';
+userOne.age = 23;
+userOne.city = "Mathara";
+userOne.username = "Kamal";
+userOne.password = "kamal";
+console.log(userOne);
+userOne.login();
+const userTwo = new SystemUser();
+userTwo.name = 'Nimal';
+userTwo.age = 27;
+userTwo.city = "Kandy";
+userTwo.username = "Nimal";
+userTwo.password = "nimal";
+console.log(userTwo);
+userTwo.register();
+let code = 123;
+let employeeCode = code;
+console.log(typeof (employeeCode));
+console.log(employeeCode);
+let employee = {};
+employee.name = 'John';
+employee.code = 123;
+console.log(employee.name);
+console.log(employee.code);
+// let someValue: unknown ="this is a string";
+// let strLength: number =(<string>someValue).length;
+// console.log(strLength);
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,58 @@
-// class SystemUser {
-//     name!: string;
-//     age!: number;
-//     city!: string;
-//     username!: string;
-//     password!: string;
+class SystemUser {
+    name!: string;
+    age!: number;
+    city!: string;
+    username!: string;
+    password!: string;
 
-//     login() {
-//         console.log('User login');
-//     }
-//     register() {
-//         console.log('User register');
-//     }
-// }
+    login() {
+        console.log('User login', this.name);
+    }
+    register() {
+        console.log('User register',this.name);
+    }
+}
 
-// const userOne = new SystemUser();
-// userOne.name = 'Kamal';
-// userOne.age = 23;
-// userOne.city = "Mathara";
-// userOne.username = "Kamal";
-// userOne.password = "kamal";
+const userOne = new SystemUser();
+userOne.name = 'Kamal';
+userOne.age = 23;
+userOne.city = "Mathara";
+userOne.username = "Kamal";
+userOne.password = "kamal";
 
-// console.log(userOne);
-// userOne.login();
-
-
-// const userTwo = new SystemUser();
-// userTwo.name = 'Nimal';
-// userTwo.age = 27;
-// userTwo.city = "Kandy";
-// userTwo.username = "Nimal";
-// userTwo.password = "nimal";
-
-// console.log(userTwo);
-// userTwo.register();
+console.log(userOne);
+userOne.login();
 
 
-// let code: any =123;
-// let employeeCode = <number>code;
-// console.log(typeof(employeeCode));
-// console.log(employeeCode);
+const userTwo = new SystemUser();
+userTwo.name = 'Nimal';
+userTwo.age = 27;
+userTwo.city = "Kandy";
+userTwo.username = "Nimal";
+userTwo.password = "nimal";
 
-// interface Employee {
-//     name:string;
-//     code:number;
-// }
+console.log(userTwo);
+userTwo.register();
 
-// let employee = <Employee>{}
-// employee.name = 'John';
-// employee.code = 123;
-// console.log(employee.name);
-// console.log(employee.code);
 
-let someValue: unknown ="this is a string";
-let strLength: number =(<string>someValue).length;
+let code: any =123;
+let employeeCode = <number>code;
+console.log(typeof(employeeCode));
+console.log(employeeCode);
 
-console.log(strLength);
+interface Employee {
+    name:string;
+    code:number;
+}
+
+let employee = <Employee>{}
+employee.name = 'John';
+employee.code = 123;
+console.log(employee.name);
+console.log(employee.code);
+
+// let someValue: unknown ="this is a string";
+// let strLength: number =(<string>someValue).length;
+
+// console.log(strLength);
 
