f40b435e14f9068b236ca9b923cf304b1f3e1a33
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-11-23 10:15:09 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -56,3 +56,77 @@ function greet(name: string): string {
 }
 console.log(greet("Alice"));
 
+
+
+// function add(a:number, b:number): number {
+//     return a+b;
+// }
+// console.log("Sum:", add(10, 20));
+
+
+
+function introduce (name: string, age?: number): string {
+    if (age !== undefined){
+        return `${name} is ${age} years old.`;
+    }
+    return `${name} chose not to share their age.`;
+}
+
+console.log(introduce("Bob", 25));
+console.log(introduce("Charlie"));
+
+//defualt parameters
+
+// function multiply(a:number, b:number = 2):number {
+//     return a*b;
+// }
+
+console.log("Multiply with default:", multiply(5));
+console.log("Multiply custom:", multiply(5,4));
+
+
+
+//arrow parametars
+
+const square = (x:number): number => x*x;
+console.log("Square:", square(6));
+
+
+//function type annotation
+let operation: (x:number, y:number) => number;
+
+//Assigning the 'add' function
+function add(a:number, b:number): number {
+    return a+b;
+}
+
+operation = add;
+console.log("Operation (Add)", operation(15,25));
+
+//Assigning the "multiply" function
+function multiply(a:number, b:number = 2): number {
+    return a*b;
+}
+
+operation = multiply;
+console.log("Operation (multiply):", operation(3,7));
+
+//Basic void Usage
+
+// Function that performs action but returns nothing
+function logMessage(message: string): void {
+    console.log(message);
+    // No return statement, or return without value
+}
+
+function displayWarning(): void {
+    alert("Warning: Something went wrong!");
+    return; // Optional explicit return without value
+}
+
+// Usage
+logMessage("Hello, World!"); // Just logs, no return value
+
+
+
+
